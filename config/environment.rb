e28111a1d4a3cde64ed3e5b96f7eece6d559457f
Robinson Regalado <rregalado17@gmail.com>
2019-09-09 02:24:14 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,4 @@ require 'pry'
 require_relative "../lib/student.rb"
 
 
-task :environment do
-  require_relative './config/environment'
-end
-
 DB = {:conn => SQLite3::Database.new("db/students.db")}
